90c402ece21ec207d3c81a4b48dbd66b181eb9d5
Yosef Meller <mellerf@netvision.net.il>
2009-03-27 11:27:30 +0300

    First test to ray handling protocol
    Tests that the flat surface class handles correctly the registering of incoming rays, returning +Infinity for missing rays and correct parametric locations for hitting rays.

--- test_flat_surface.py
+++ test_flat_surface.py
@@ -1,5 +1,10 @@
-import unittest.py 
+import unittest
+import math
+import numpy as N
+from scipy import c_, r_
+
 from flat_surface import FlatSurface
+from ray_bundle import RayBundle
 
 class TestFlatSurfaceInterface(unittest.TestCase):
     def runTest(self):
@@ -7,3 +12,23 @@ class TestFlatSurfaceInterface(unittest.TestCase):
         surf = FlatSurface()
         self.assertRaises(ValueError, surf.set_width, -42)
         self.assertRaises(ValueError, surf.set_height, -42)
+
+class TestTraceProtocol(unittest.TestCase):
+    def setUp(self):
+        self._surf = FlatSurface()
+        
+        dir = N.array([[1, 1, -1], [-1, 1, -1], [-1, -1, -1], [1, -1, -1]]).T / math.sqrt(3)
+        position = c_[[0,0,1], [1,-1,1], [1,1,1], [-1,1,1]]
+        
+        self._bund = RayBundle()
+        self._bund.set_vertices(position)
+        self._bund.set_directions(dir)
+        
+    def test_register_incoming(self):
+        """A simple bundle is registered correctly"""
+        correct_params = r_[[math.sqrt(3)]*3]
+        params = self._surf.register_incoming(self._bund)
+        
+        self.failUnless(params[0] == N.inf)
+        N.testing.assert_array_almost_equal(params[1:], correct_params)
+        
