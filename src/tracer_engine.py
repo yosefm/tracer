df6204787f75ac241849423d97cacc9149129583
Jasmine Florentine <jasminef@mit.edu>
2009-06-09 16:44:55 +0300

    Added tracer_engine.py and the unit tests for it. tracer_engine currently only works for single iterations of the simulation, but can otherwise trace ray bundles on multiple flat surfaces.

--- src/tracer_engine.py
+++ src/tracer_engine.py
@@ -2,9 +2,9 @@
 
 import numpy as N
 from flat_surface import FlatSurface 
-import ray_bundle
-import optics
-import spatial_geometry
+from ray_bundle import RayBundle
+from ray_bundle import solar_disk_bundle
+import pylab as P
 
 class TracerEngine():
     """
@@ -18,107 +18,93 @@ class TracerEngine():
         """
         self.objects = objects
 
-    #def intersect_ray(self,bundle, source):
+    def intersect_ray(self, bundle):
         """
-        Intersects a ray bundle with all the objects and determines which intersection
-        occurred closest, and therefore which intersection to consider for further 
-        computations
-        Arguments:
-        bundle - the ray bundle
-        objects - list of objects
-        source - starting point of the ray
-        Returns:
-        List of coordinates for each ray intersection
+        Returns objs_param, a list of parameters of intersections for each object, with
+        misses replaces by infinity. Filters out any intersections along a ray that
+        aren't the intersection with the closest object it hits
         """
+        # If there is only a single object, don't need to find minimum distance
+        if len(self.objects) == 1:
+            objs_param = [self.objects[0].register_incoming(bundle)]
+
+        else:
+            params_final = []
+            # Bounce rays off each object
+            for obj in self.objects:
+                if self.objects.index(obj) == 0:
+                    stack = N.r_[obj.register_incoming(bundle)]
+                    objs_hit = N.c_[obj]
+                else:
+                    objs_hit = N.vstack((objs_hit, obj))
+                    stack = N.vstack((stack, obj.register_incoming(bundle)))
     
-        # checks each individual ray against all the objects
-        #hits = []
-        #for ray in range(bundle.get_num_rays()):
-        #    distances = []
-        #    coords = []
-        #    for obj in self.objects:
-        #        # find the distance from the source to the intersection
-        #        hit = obj.register_incoming(bundle)  # wrong, need to do each ray individually
-        #        dist = N.linalg.norm(source[:,ray] - hit)
+            # Raise an error if any of the parameters are negative
+            if (stack < 0).any():
+                raise ValueError("Parameters must all be positive")
 
-        #    distances.append(dist)
-        #    coords.append(hit)
-        
-        #    # determine which intersection occurred closest
-        #    i = N.argmin(distances)
-        #    hits.append(coords[i])
+            # Find the smallest parameter for each ray, and use that as the final one
+            params_index = stack.argmin(axis=0)
 
-        #return hits
+            objs_param = []
+            for obj in range(N.shape(objs_hit)[0]):
+                obj_array = N.where(params_index == obj)
+                obj_row = stack[obj]
+                if N.shape(obj_array[0]) != N.shape(obj_row):
+                    obj_row[~obj_array[0]] = N.inf
+                objs_param.append(obj_row)
 
-    # The above is incorrect, what needs to happen:
+        return objs_param
 
-    """
-    1) bounce a bundle off every object individually
-    2) for each object, there is now a list of params for where each ray hit
-    3) from each of the params per object, choose the smallest distance
-    to be the point of intersection (or rather, index to the same point on 
-    all the different list of params and take the params at each of those
-    indices and compare)
-    4) result should be a list of intersections with different objects, but the
-    first intersection of each ray
-    """
-
-    def intersect_ray(self, bundle, source):
-        hits = []
-        coords = []
-        for obj in self.objects:
-            hits.append(obj.register_incoming(bundle).get_vertices())
-
-        for ray in range(bundle.get_num_rays()):
-            dists = []
-            for list in hits:
-                dist = N.linalg.norm(source[:,ray] - list[ray]) 
-                dists.append(dist)
-        
-            i = N.argmin(dists)
-            coords.append(hits[i])
-
-        return coords
-
-    def bundle_driver(self,energy, num_rays, center, direction, radius, ang_range):
+    def bundle_driver(self, bundle, reps):
         """
         Creates a ray bundle or uses a reflected ray bundle, and intersects it with all
         objects, uses intersect_ray()
+        Arguments:
+        reps - number of times to repeat the simulation (where each simulation represents
+        a ray bundle being intersected with a set of objects one time)
+        Returns:
+        For the time being, it v, an array of vertices of the most recent intersections
         """
-        # First set of rays is from the initial source
-        bund = ray_bundle.solar_disk_bundle(num_rays, center, direction, radius,
-                                            ang_range)
-        coords = intersect_ray(bund, bund.get_vertices)
-        inters = ~N.isinf(coords)
 
-        # After this first hit, all other rays are reflected
-        for i in range(100):  #NEED TO HAVE A LIMIT TO THE LOOP
-            coords = intersect_ray(outg, outg.get_vertices()) 
-            inters = ~N.isinf(coords)
-            outg = obj.get_outgoing(inters)
-        
+        energy = bundle.get_energy()
+        for i in xrange(reps):  
+            # If this is the first ray, use initial source
+            if i == 0:
+                bund = bundle
+
+            # Else, use outgoing rays from previous intersection
+            else: 
+                bund = outg
+
+            objs_param = self.intersect_ray(bund)
+
+            for obj in self.objects:
+                obj_hit = objs_param[self.objects.index(obj)]
+                inters = ~N.isinf(obj_hit)
+                if self.objects.index(obj) == 0:
+                    outg = obj.get_outgoing(inters)
+                    outg.set_energy(energy[:,inters])
+                else:
+                    new_outg = obj.get_outgoing(inters)
+                    new_outg.set_energy(energy[:,inters]) 
+                    outg = outg + new_outg
+                                   
         # Non-intersecting rays
-        not_v = bund.get_vertices()[:, ~inters]
-        not_d = bund.get_directions()[:, ~inters]
+        v = bund.get_vertices()[:, ~inters]
+        d = bund.get_directions()[:, ~inters]
+        #P.quiver(v[0], v[1], d[0], d[1], scale=0.1)
         
         # Returning rays
         v = outg.get_vertices()
         d = outg.get_directions()
+        #P.quiver(v[0], v[1], d[0], d[1], scale=0.1, color='red')
 
-        # NEEDS TO KEEP TRACK OF ENERGY TOO
-
-    def recieved(self):
-        """
-        Shows the energy and distribution of rays on the recieving surface
-        """
+        #P.show()        
+        
+        return v
                       
 
 
-flat1 = FlatSurface()
-direction = N.array([0., 0, -1])
-center = N.array([0,0,2]).reshape(-1,1)
-sun = ray_bundle.solar_disk_bundle(5000,center, direction, 2, N.pi/1000.) 
-engine = TracerEngine([flat1])
-engine.intersect_ray(sun, sun.get_vertices())
 
 
---
+++ tests/test_tracer_engine.py
@@ -0,0 +1,135 @@
+import unittest
+import numpy as N
+import math
+
+from tracer_engine import TracerEngine
+from ray_bundle import RayBundle
+from flat_surface import FlatSurface
+from spatial_geometry import general_axis_rotation
+
+class TestTraceProtocol1(unittest.TestCase):
+    """ 
+    Tests intersect_ray and the bundle driver with a single flat surface, not rotated, with 
+    a single interation 
+    """
+    def setUp(self):
+        
+        dir = N.array([[1,1,-1],[-1,1,-1],[-1,-1,-1],[1,-1,-1]]).T/math.sqrt(3)
+        position = N.c_[[0,0,1],[1,-1,1],[1,1,1],[-1,1,1]]
+       
+        self._bund = RayBundle()
+        self._bund.set_vertices(position)
+        self._bund.set_directions(dir)
+        energy = N.array([1,1,1,1])
+        self._bund.set_energy(energy)
+
+        objects = [FlatSurface()]
+        self.engine = TracerEngine(objects)
+
+    def test_intersect_ray1(self):
+        correct_params = N.r_[[math.sqrt(3)]*3]
+        params = self.engine.intersect_ray(self._bund)[0]
+
+        self.failUnless(params[0] == N.inf)
+        N.testing.assert_array_almost_equal(params[1:], correct_params)
+
+    def test_bundle_driver(self):
+        params = self.engine.bundle_driver(self._bund,1)
+        correct_params = N.c_[[0,0,0],[0,0,0],[0,0,0]]
+
+        N.testing.assert_array_almost_equal(params, correct_params)
+
+class TestTraceProtocol2(unittest.TestCase):
+    """
+    Tests intersect_ray with a flat surface rotated around the x axis 45 degrees
+    """
+    def setUp(self):
+        
+        dir = N.c_[[0,0,1],[0,0,-1],[0,-1,-1]]
+        position = N.c_[[0,0,1],[0,1,2],[0,0,1]]
+
+        self._bund = RayBundle()
+        self._bund.set_vertices(position)
+        self._bund.set_directions(dir)
+
+    def test_intersect_ray2(self):
+        rot = general_axis_rotation([1,0,0],N.pi/4)
+        objects = [FlatSurface(rotation=rot,width=4,height=4)]
+        
+        engine = TracerEngine(objects)
+        params = engine.intersect_ray(self._bund)[0]
+        correct_params = N.r_[N.inf, 1, N.inf]
+
+        self.failUnless(params[0] and params[2] == N.inf)
+        N.testing.assert_array_almost_equal(params[1], correct_params[1])
+
+class TestTraceProtocol3(unittest.TestCase):
+    """
+    Tests intersect_ray and the bundle driver with two rotated planes, with a single iteration
+    """
+    def setUp(self):
+        
+        self.x = 1/(math.sqrt(2))
+        dir = N.c_[[0,self.x,-self.x],[0,1,0]]
+        position = N.c_[[0,0,1],[0,0,1]]
+
+        self._bund = RayBundle()
+        self._bund.set_vertices(position)
+        self._bund.set_directions(dir)
+
+        rot1 = general_axis_rotation([1,0,0],N.pi/4)
+        rot2 = general_axis_rotation([1,0,0],N.pi/(-4))
+        objects = [FlatSurface(rotation=rot1,width=4,height=4), FlatSurface(rotation=rot2,width=4,height=4)]
+        energy = N.array([1,1])
+        self._bund.set_energy(energy)
+
+        self.engine = TracerEngine(objects)
+        
+    def test_intersect_ray(self):
+        params = self.engine.intersect_ray(self._bund)
+        correct_params = N.array([self.x,1])
+
+        self.failUnless(params[1][0] and params[1][1] == N.inf) 
+        N.testing.assert_array_almost_equal(params[0],correct_params)    
+
+    def test_bundle_driver1(self):
+        params = self.engine.bundle_driver(self._bund, 1)
+        correct_params = N.c_[[0,.5,.5],[0,1,1]]
+
+        N.testing.assert_array_almost_equal(params,correct_params)
+
+class TestTraceProtocol4(unittest.TestCase):
+    """
+    Tests intersect_ray and the bundle driver with two planes, where the rays hit different surfaces
+    """
+    def setUp(self):
+
+        self.x = 1/(math.sqrt(2))
+        dir = N.c_[[0,-self.x,self.x],[0,0,-1]]
+        position = N.c_ [[0,2,1],[0,2,1]]
+
+        self._bund = RayBundle()
+        self._bund.set_vertices(position)
+        self._bund.set_directions(dir)
+
+        rot1 = general_axis_rotation([1,0,0],N.pi/4)
+        energy = N.array([1,1])
+        self._bund.set_energy(energy)
+        objects = [FlatSurface(rotation=rot1,width=10,height=10), FlatSurface(width=10,height=10)]
+        self.engine = TracerEngine(objects)
+
+        
+    def test_bundle_driver1(self):
+        params = self.engine.bundle_driver(self._bund, 1)
+        correct_params = N.c_[[0,1.5,1.5],[0,2,0]]
+
+        N.testing.assert_array_almost_equal(params,correct_params)
+'''
+    def test_bundle_driver2(self):
+        params = self.engine.bundle_driver(self._bund, 2)
+        correct_params = N.c_[[0,2,1],[0,2.5,0]]
+
+        N.testing.assert_array_almost_equal(params,correct_params)
+'''
+if __name__ == '__main__':
+    unittest.main()
