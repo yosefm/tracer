e52bdb38b888be7df30b29dedd8161f918dd5cd6
Jasmine Florentine <jasminef@mit.edu>
2009-06-16 16:06:52 +0300

    Added a receiver surface.

---
+++ src/receiver.py
@@ -0,0 +1,52 @@
+# Implements a surface specifically as a recieving surface                                     
+
+import numpy as N
+import pylab as P
+from flat_surface import FlatSurface
+from ray_bundle import RayBundle
+import optics
+
+class Receiver(FlatSurface):
+    """                                                                                        
+    Implements a flat recieving surface for the rays                                           
+    """
+    def __init__(self, location=None, rotation=None, absorptivity=1.,width=1.,height=1.):
+        FlatSurface.__init__(self, location, rotation, absorptivity, width, height)
+        self.coordinates = []
+        self.energy = []
+
+    def get_outgoing(self, selector, energy, parent):
+        vertices = N.dot(self.get_rotation()[:, :2],  self._current_params[:, selector]) + \
+            self.get_location()[:, None]
+        dirs = optics.reflections(self._current_bundle.get_directions()[:, selector],
+            self.get_rotation()[:, 2][:,None])
+        outg = RayBundle()
+        outg.set_vertices(vertices)
+        outg.set_directions(dirs)
+        outg.set_energy(energy[:,selector])
+        new_parent = parent[selector]
+        outg.set_parent(new_parent)
+        self.collect_energy(outg)
+        return outg
+
+    def collect_energy(self, bundle):
+        """                                                                                    
+        Saves the values of the coordinates and energy of incoming rays                        
+        """
+        self.coordinates.append(bundle.get_vertices())
+        self.energy.append(bundle.get_energy())
+  
+    def plot_energy(self):
+        """                                                                                    
+        Plots the energy distribution on the receiving surface                                 
+        """
+        coords = self.coordinates[0]
+       # rot = N.array([[1,0,0],[0,0.7,0.7],[0,0.7,-0.7]])
+        coords_rot = N.dot(self.get_rotation(), coords)
+        energy = N.array(self.energy)
+        x = coords_rot[0]  # this should be by row is there is more than one
+        y = coords_rot[1]  # receiving surface; also this is the local x, y
+                
+        P.scatter(x, y)
+        P.show()
+
---
+++ tests/test_tracer_engine_energy.py
@@ -0,0 +1,46 @@
+import unittest
+import numpy as N
+import math
+
+from tracer_engine import TracerEngine
+import ray_bundle
+from flat_surface import FlatSurface
+from spatial_geometry import general_axis_rotation
+from receiver import Receiver
+
+
+class TestTraceProtocol4(unittest.TestCase):
+    """
+    Tests intersect_ray and the bundle driver with two planes, where the rays hit different 
+    surfaces
+    """
+    def setUp(self):
+
+        self.x = 1/(math.sqrt(2))
+        dir = N.array([0,0,-1])
+        position = N.array([0,2,1]).reshape(-1, 1)
+
+        self.bund = ray_bundle.solar_disk_bundle(5000, position, dir, 2, N.pi/1000.)
+
+        rot1 = general_axis_rotation([1,0,0],N.pi/4)
+        energy = N.ones(5000)
+        self.bund.set_energy(energy)
+        self.objects = [Receiver(rotation=rot1,width=10,height=10), 
+                   FlatSurface(width=10,height=10)]
+        self.engine = TracerEngine(self.objects)
+        
+    def test_ray_tracer1(self):
+
+        self.engine.ray_tracer(self.bund, 1)
+        self.objects[0].plot_energy()
+
+
+"""
+    def test_ray_tracer2(self):
+        params = self.engine.ray_tracer(self._bund, 2)
+        correct_params = N.c_[[0,2,2],[0,3,0]]
+
+        N.testing.assert_array_almost_equal(params,correct_params)
+"""
+if __name__ == '__main__':
+    unittest.main()
