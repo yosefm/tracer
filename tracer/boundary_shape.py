566b69ee93ff71347c4f4eb263e06114b3d09564
Yosef Meller <mellerf@netvision.net.il>
2009-08-19 14:26:26 +0300

    More code-sharing between Surface and BoundaryShape

--- tracer/boundary_shape.py
+++ tracer/boundary_shape.py
@@ -1,22 +1,47 @@
 # Implements a sphere as a bounding shape to a surface to define the exact shape of the surface
 
 import numpy as N
+from has_frame import HasFrame
 
-class BoundarySphere():
+class BoundaryShape(HasFrame):
+    """
+    Represent a surface that encloses a volume, so that it is possible to check
+    whether certain points are within that volume.
+    """
+    def __init__(self, location=None, rotation=None):
+        """
+        Arguments:
+        location - a 1D array of 3 components, representing the 3D location of 
+            the surface's frame's origin, in the containing object's frame.
+        rotation - a 3x3 array representing the rotation matrix of the surface's 
+        """
+        HasFrame.__init__(self, location, rotation)
+    
+    def in_bounds(self, points):
+        """
+        Every subclass must implement a function that says which points are in 
+        the enclosed volume.
+        
+        Arguments: 
+        points - a 3 by n array for n 3D points
+        
+        Returns: 
+        an n-length 1D boolean array stating for each point whether it is in 
+        the bounds or not.
+        """
+        raise TypeError("Virtual function in_bounds() called. Implement " + \
+            "this in a derived class")
 
+class BoundarySphere(BoundaryShape):
     def __init__(self, location=None,  radius=1.):
-        """Arguments:
-        location - a 1D array
+        """
+        Arguments:
         radius - a float
+        
         Attributes:
-        _loc - the location of the center
-        _temp_loc - the location of the center if it has been transformed; stored temporarily
-        for calculations
         _radius - radius of the bounding sphere
         """
-        if location == None:
-            location = N.zeros(3)
-        self._loc = location
+        BoundaryShape.__init__(self, location, None)
         self._temp_loc = self._loc
         self._radius = radius
             
--- tracer/surface.py
+++ tracer/surface.py
@@ -8,54 +8,14 @@
 # All surfaces are grey.
 
 import numpy as N
+from has_frame import HasFrame
 
-class Surface(object):
-    """Defines the base of surfaces that interact with rays.
-    Each surface has a location vector and a rotation matrix. Together they 
-    define the frame of reference of the object. The rotation and location are
-    accessible from the _rot and _loc attributes, respectively, or from the
-    appropriate places in the homogenous transformation stored in _transform
-    and kept in sync with _rot and _loc.
-    A surface may construct its own local coordinates from the vectors of the 
-    rotation matrix.
-    The rotation matrix is from the global to the local, so its columns are the 
-    basis of the local coordinates, written in the global coordinates. See [1] p. 25
-    
-    Tentative transformations are held in _temp_frame, which allows to preserve the 
-    relative transform while holding a global transform.
+class Surface(HasFrame):
     """
-    def __init__(self,  location=None,  rotation=None):
-        # default location and rotation:
-        if location is None:
-            location = N.zeros(3)
-        if rotation is None:
-            rotation = N.eye(3)
-        
-        self._transform = N.empty((4,4))
-        self._transform[3,:] = N.r_[0, 0, 0, 1]
-        self.set_location(location)
-        self.set_rotation(rotation)
-        self._temp_frame = self._transform
-    
-    def get_location(self):
-        return self._loc
-    
-    def get_rotation(self):
-        return self._rot
-    
-    def set_location(self,  location):
-        """Sets the location within the object"""
-        if location.shape != (3, ):
-            raise ValueError("location must be a 1D 3-component array")
-        self._loc = location
-        self._transform[:3,3] = location
-    
-    def set_rotation(self,  rotation):
-        """Sets the rotation within the object"""
-        if  rotation.shape != (3, 3):
-            raise ValueError("rotation must be a 3x3 array")
-        self._rot = rotation
-        self._transform[:3,:3] = rotation
+    Defines the base of surfaces that interact with rays.
+    """
+    def __init__(self, location=None, rotation=None):
+        HasFrame.__init__(self, location, rotation)
 
     def set_parent_object(self, object):
         """Describes which object the surface is in """
@@ -75,25 +35,6 @@ class Surface(object):
     def get_outer_n(self):
         return self._outer_n
     
-    def set_transform(self, transform):
-        """Defines the transformation matrix the puts the surface into the coordinates of the
-        object containing the surface (the parent object).
-        Arguments: 
-        transform - a 2D array defining the 4 by 4 transformation matrix."""   
-        self._transform = transform
-        self._loc = self._transform[:3,3]
-        self._rot = self._transform[:3,:3]
-
-    def get_transform(self):
-        return self._transform
-
-    def transform_frame(self, transform): 
-        """Updates the transformation matrix that puts the surface into the global
-        coordinates. I.e., if the object the surface is in is rotated, than the surface
-        is also rotated.  It then defines a temporary rotated frame for use of 
-        calculations.""" 
-        self._temp_frame = N.dot(transform, self._transform)
-        
     def get_ref_index(self, n, bund, selector):
         """                                                                                  
         Determines which refractive index to use based on the refractive index               
