81793c4116b22f6513c9ef9fbbbc1a64ab4fe2ed
Yosef Meller <mellerf@netvision.net.il>
2009-10-24 18:02:15 +0200

    Prepare quadric surface for a more flexible sphere (or other quadric) handling, that lets holes etc. be programmed separately.

--- tracer/quadric.py
+++ tracer/quadric.py
@@ -184,29 +184,23 @@ class QuadricGM(GeometryManager):
             else: hits = (-B[ray] + N.r_[-1, 1]*N.sqrt(delta[ray]))/(2*A[ray])
             coords = vertex + d[:,ray]*hits[:,None]
             
-            is_positive = N.where(hits > 0)[0]
+            # Quadrics can have two intersections. Here we allow child classes
+            # to choose based on own method:
+            select = self._select_coords(coords, hits)
             
-            # If both are negative, it is a miss
-            if N.shape(is_positive) == (0,):
+            # Returning None from _select_coords() means the ray missed anyway:
+            if select is None:
                 params.append(N.inf)
                 vertices.append(N.empty([3,1]))
                 norm.append(N.empty([3,1]))
                 continue
-              
-            # If both are positive, us the smaller one
-            if len(is_positive) == 2:
-                param = N.argmin(hits)
             
-            # If either one is negative, use the positive one
-            else:
-                param = is_positive[0]
-
-            verts = N.c_[coords[param,:]]
+            verts = N.c_[coords[select,:]]
             
-            dot = N.vdot(c.T - coords[param,:], d[:,ray])
-            normal = self.get_normal(dot, coords[param,:], c)
+            dot = N.vdot(c.T - coords[select,:], d[:,ray])
+            normal = self.get_normal(dot, coords[select,:], c)
             
-            params.append(hits[param])
+            params.append(hits[select])
             vertices.append(verts)
             norm.append(normal)
             
@@ -217,6 +211,37 @@ class QuadricGM(GeometryManager):
         
         return N.array(params)
     
+    def _select_coords(self, coords, prm):
+        """
+        Choose between two intersection points on a quadric surface.
+        This is a default implementation that takes the first positive-
+        parameter intersection point.
+        
+        The default behaviour is to take the first intersection not behind the
+        ray's vertex (positive prm).
+        
+        Arguments:
+        coords - a 2x3 array whose each row is the global coordinates of one
+            intersection point of a ray with the sphere.
+        prm - the corresponding parametric location on the ray where the 
+            intersection occurs.
+        
+        Returns:
+        The index of the selected intersection, or None if neither will do.
+        """
+        is_positive = N.where(prm > 0)[0]
+
+        # If both are negative, it is a miss
+        if len(is_positive) == 0:
+            return None
+        
+        # If both are positive, us the smaller one
+        if len(is_positive) == 2:
+            return N.argmin(prm)
+        else:
+            # If either one is negative, use the positive one
+            return is_positive[0]
+        
     def get_normals(self, selector):
         """
         Report the normal to the surface at the hit point of selected rays in
