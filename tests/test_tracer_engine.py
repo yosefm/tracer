ea6586efd6c35215677f6861d32bf44b9ae3d71f
Yosef Meller <mellerf@netvision.net.il>
2009-10-27 09:13:32 +0200

    Convert more tests to the new surface

--- tests/test_tracer_engine.py
+++ tests/test_tracer_engine.py
@@ -11,8 +11,9 @@ from boundary_shape import BoundarySphere
 from receiver import Receiver
 from assembly import Assembly
 from object import AssembledObject
-from surface import Surface
 
+from surface import Surface
+from flat_surface import FlatGeometryManager
 import optics_callables as opt
 
 class TestTraceProtocol1(unittest.TestCase):
@@ -21,7 +22,6 @@ class TestTraceProtocol1(unittest.TestCase):
     a single interation 
     """
     def setUp(self):
-        
         dir = N.array([[1,1,-1],[-1,1,-1],[-1,-1,-1],[1,-1,-1]]).T/math.sqrt(3)
         position = N.c_[[0,0,1],[1,-1,1],[1,1,1],[-1,1,1]]
        
@@ -33,22 +33,21 @@ class TestTraceProtocol1(unittest.TestCase):
         
         self.assembly = Assembly()
         object = AssembledObject()
-        object.add_surface(FlatSurface())
+        object.add_surface(Surface(FlatGeometryManager(), opt.perfect_mirror))
         self.assembly.add_object(object)
         self.engine = TracerEngine(self.assembly)
         
     def test_intersect_ray1(self):
-        correct_params = N.r_[[False, True, True, True]]
         params = self.engine.intersect_ray(self._bund)[0]
-
-        N.testing.assert_array_almost_equal(params, correct_params)
+        self.failUnless(params.all())
 
     def test_ray_tracer(self):
-        
+        """Ray tracer after one iteration returns what the surface would have"""
         params = self.engine.ray_tracer(self._bund,1,.05)[0]
-        correct_params = N.c_[[0,0,0],[0,0,0],[0,0,0]]
+        correct_pts = N.zeros((3,4))
+        correct_pts[:2,0] = 1
         
-        N.testing.assert_array_almost_equal(params, correct_params)
+        N.testing.assert_array_almost_equal(params, correct_pts)
 
 class TestTraceProtocol2(unittest.TestCase):
     """
@@ -66,7 +65,7 @@ class TestTraceProtocol2(unittest.TestCase):
 
     def test_intersect_ray2(self):
         rot = general_axis_rotation([1,0,0],N.pi/4)
-        surface = FlatSurface(rotation=rot,width=4,height=4)
+        surface = Surface(FlatGeometryManager(), opt.perfect_mirror, rotation=rot)
         assembly = Assembly()
         object = AssembledObject()
         object.add_surface(surface)
@@ -74,7 +73,7 @@ class TestTraceProtocol2(unittest.TestCase):
         
         engine = TracerEngine(assembly)
         params = engine.intersect_ray(self._bund)[0]
-        correct_params = N.r_[[False, True, False]]
+        correct_params = N.r_[[False, True, True]]
 
         N.testing.assert_array_almost_equal(params, correct_params)
 
@@ -83,7 +82,6 @@ class TestTraceProtocol3(unittest.TestCase):
     Tests intersect_ray and the bundle driver with two rotated planes, with a single iteration
     """
     def setUp(self):
-        
         self.x = 1/(math.sqrt(2))
         dir = N.c_[[0,self.x,-self.x],[0,1,0]]
         position = N.c_[[0,0,1],[0,0,1]]
@@ -92,13 +90,14 @@ class TestTraceProtocol3(unittest.TestCase):
         self._bund.set_vertices(position)
         self._bund.set_directions(dir)
         self._bund.set_ref_index(N.r_[[1,1,1]])
+        energy = N.array([1,1])
+        self._bund.set_energy(energy)
 
         rot1 = general_axis_rotation([1,0,0],N.pi/4)
         rot2 = general_axis_rotation([1,0,0],N.pi/(-4))
-        surf1 = FlatSurface(rotation=rot1,width=4,height=4)
-        surf2 = FlatSurface(rotation=rot2,width=4,height=4)
-        energy = N.array([1,1])
-        self._bund.set_energy(energy)
+        surf1 = Surface(FlatGeometryManager(), opt.perfect_mirror, rotation=rot1)
+        surf2 = Surface(FlatGeometryManager(), opt.perfect_mirror, rotation=rot2)
+        
         assembly = Assembly()
         object = AssembledObject()
         object.add_surface(surf1)
@@ -138,8 +137,8 @@ class TestTraceProtocol4(unittest.TestCase):
         energy = N.array([1,1])
         self._bund.set_energy(energy)
 
-        surf1 = FlatSurface(rotation=rot1, width=10,height=10)
-        surf2 = FlatSurface(width=10,height=10)
+        surf1 = Surface(FlatGeometryManager(), opt.perfect_mirror, rotation=rot1)
+        surf2 = Surface(FlatGeometryManager(), opt.perfect_mirror)
         assembly = Assembly()
         object = AssembledObject()
         object.add_surface(surf1)
@@ -161,7 +160,7 @@ class TestTraceProtocol4(unittest.TestCase):
         N.testing.assert_array_almost_equal(params,correct_params)
     
     def test_too_much_intersections(self):
-	"""The tracer stops when all rays are escaping"""
+        """The tracer stops when all rays are escaping"""
         params = self.engine.ray_tracer(self._bund, 42, 0.05)[0]
         correct_params = N.array([]).reshape(3,0)
 
@@ -198,10 +197,12 @@ class TestTraceProtocol6(unittest.TestCase):
     Tests a spherical surface
     """
     def setUp(self):
-        boundary1 = BoundarySphere(N.array([0,2.,0]),3.)
-        surface1 = SphereSurface(location=N.array([0,0,0]), radius=2)
-        boundary2 = BoundarySphere(N.array([0,-5,0]),3)
-        surface2 = SphereSurface(location=N.array([0,-2,0]), radius=2)
+        surface1 = Surface(HemisphereGM(2.), opt.perfect_mirror,
+            rotation=general_axis_rotation(N.r_[1,0,0], N.pi/2.))
+        surface2 = Surface(HemisphereGM(2.), opt.perfect_mirror, 
+            location=N.array([0,-2,0]), 
+            rotation=general_axis_rotation(N.r_[1,0,0], -N.pi/2.))
+        
         self._bund = RayBundle()
         self._bund.set_directions(N.c_[[0,1,0]])
         self._bund.set_vertices(N.c_[[0,-1,0]])
@@ -213,8 +214,6 @@ class TestTraceProtocol6(unittest.TestCase):
         object2 = AssembledObject()
         object1.add_surface(surface1)
         object2.add_surface(surface2)
-        object1.add_boundary(boundary1)
-        object2.add_boundary(boundary2)
         assembly.add_object(object1)
         assembly.add_object(object2)
 
@@ -225,34 +224,6 @@ class TestTraceProtocol6(unittest.TestCase):
         correct_params = N.c_[[0,2,0]]
 
         N.testing.assert_array_almost_equal(params,correct_params)
-
-class TestRefraction(unittest.TestCase):
-    """Tests refractive properties of a flat surface""" 
-    def setUp(self):
-        self.x = 1/(math.sqrt(2))
-        dir = N.c_[[0,0,-1],[0,-self.x,-self.x]]
-        position = N.c_[[0,0,1],[0,1,1]]
-
-        self._bund = RayBundle()
-        self._bund.set_vertices(position)
-        self._bund.set_directions(dir)
-        self._bund.set_energy(N.r_[[1.,1.]])
-        self._bund.set_ref_index(N.r_[[1.,1.]])  
-        
-        assembly = Assembly()
-        object = AssembledObject()
-        object.add_surface(FlatSurface(mirror=False))
-        object.set_ref_index(object.get_surfaces(), 1.5)
-        assembly.add_object(object)
-
-        self.engine = TracerEngine(assembly)
-                       
-    def test_intersect_ray1(self):  
-        correct_params = N.r_[0, .4908826, -0.785398 ]  
-        ans = self.engine.ray_tracer(self._bund, 1, .05)
-        params = N.arctan(ans[1][1]/ans[1][2])
-        N.testing.assert_array_almost_equal(params, correct_params)
-  
 
 if __name__ == '__main__':
     unittest.main()
