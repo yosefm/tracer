3f565ae4c57aafeb5f1b9e9ff589ad084d4f7e28
Yosef Meller <mellerf@netvision.net.il>
2009-12-13 20:20:21 +0200

    A receiver plate model, tested by the resurrected example in examples.py

--- examples/test_case.py
+++ examples/test_case.py
@@ -1,62 +1,74 @@
 import numpy as N
 import math
+import pylab as P
 
 from tracer.tracer_engine import TracerEngine
 from tracer.ray_bundle import RayBundle, solar_disk_bundle
-from tracer.flat_surface import FlatSurface
-from tracer.spatial_geometry import general_axis_rotation, generate_transform
-from tracer.sphere_surface import SphereSurface
+from tracer.spatial_geometry import rotx, generate_transform
+
 from tracer.boundary_shape import BoundarySphere
-from tracer.receiver import Receiver
 from tracer.assembly import Assembly
 from tracer.object import AssembledObject
-from tracer.paraboloid import Paraboloid
+from tracer.surface import Surface
 
+from tracer.models.one_sided_mirror import one_sided_receiver
+from tracer.paraboloid import ParabolicDishGM
+import tracer.optics_callables as opt
+
+def plot_hits(x, y, energy, bins=100):
+    """
+    Generates a 2D histogram of hits by their x,y coordinates, weighted by the
+    energy absorbed in each hit. Plots the histogram using Pylab.
+    
+    Returns:
+    f - a pylab figure object with the histogram on it, not shown yet.
+    """
+    H, xbins, ybins = N.histogram2d(x, y, bins, weights=energy)
+    f = P.figure()
+    P.imshow(H.T)
+    P.colorbar()
+    return f
+    
 def test_case(focus):
+    # Case parameters (to be moved out:
     w = 10.
     h = 10.
-    a = 2*N.sqrt(focus)
-    b = a
+    D = 5.
     
-    bound_radius = 3.
-    bound_center = N.array([0,0.,0])
-    transform1 = generate_transform(N.array([1.,0,0]), 3*N.pi/4, N.c_[[0, 2.5, 2.5]])
-    transform2 = generate_transform(N.array([1.,0,0]), -N.pi/4, N.c_[[0,0.,0]])  
     num_rays = 10000
     center = N.c_[[0, 2., 2.]]
     x = -1/(math.sqrt(2))
     direction = N.array([0,x,x])
-    radius_sun = 5. 
+    radius_sun = 2.5 
     ang_range = 0.0005
     iterate = 2
     min_energy = 0.05
-
-    assembly = Assembly()
-    surface1 = Receiver(width=w, height=h)
-    rot1 = general_axis_rotation(N.array([0,0,1]), N.pi/2)
-    surface2 = FlatSurface(location=N.array([0,2,1.]), rotation=rot1, width=w, height=h)
-    surface3 = FlatSurface(location=N.array([0,-2,1.]), rotation=rot1, width=w, height=h)
-    object1 = AssembledObject()
-    object1.add_surface(surface1)
-    #object1.add_surface(surface2)
-    #object1.add_surface(surface3)
     
-    surface4 = Paraboloid(a=a, b=b)
-    bound1 = BoundarySphere(location=bound_center, radius=bound_radius)
-    object2 = AssembledObject()
-    object2.add_surface(surface4)
-    object2.add_boundary(bound1)
+    # Model:
+    receiver, rec_obj = one_sided_receiver(w, h)
+    receiver_frame = generate_transform(N.r_[1.,0,0], 3*N.pi/4., N.c_[[0, 2.5, 2.5]])
+    rec_obj.set_transform(receiver_frame)
     
-    assembly.add_object(object1, transform1)
-    assembly.add_object(object2, transform2)
+    dish_surf = Surface(ParabolicDishGM(D, focus), opt.perfect_mirror)
+    dish = AssembledObject(surfs=[dish_surf])
+    dish.set_transform(rotx(-N.pi/4.))
     
+    assembly = Assembly(objects=[rec_obj, dish])
+    
+    # Rays:
     sun = solar_disk_bundle(num_rays, center, direction, radius_sun, ang_range)
     sun.set_energy(N.ones(num_rays))
     sun.set_ref_index(N.ones(num_rays))
     
+    # Do the tracing:
     engine = TracerEngine(assembly)
     engine.ray_tracer(sun, iterate, min_energy)
-    surface1.plot_energy()
+    
+    # Plot:
+    energy, pts = receiver.get_optics_manager().get_all_hits()
+    x, y = receiver.get_geometry_manager().global_to_local(pts)[:2]
+    f = plot_hits(x, y, energy)
+    f.show()
 
 if __name__ == '__main__':
     import optparse
@@ -65,4 +77,4 @@ if __name__ == '__main__':
     parser.add_option('--focus', '-f', dest='foc', type='float', default=6.25)
     opts, pos = parser.parse_args()
     test_case(opts.foc)
-
+    P.show()
--- tracer/models/one_sided_mirror.py
+++ tracer/models/one_sided_mirror.py
@@ -7,7 +7,7 @@ import optics_callables as opt
 
 from numpy import r_
 
-def rect_one_sided_mirror(width, height, absorptivity):
+def rect_one_sided_mirror(width, height, absorptivity=0.):
     """
     construct an object with two surfaces: one on the XY plane, that is
     specularly reflective, and one slightly below (negative z), that is opaque.
@@ -24,3 +24,28 @@ def rect_one_sided_mirror(width, height, absorptivity):
         location=r_[0., 0., -1e-10])
     obj = AssembledObject(surfs=[front, back])
     return obj
+
+def one_sided_receiver(width, height, absorptivity=1.):
+    """
+    construct an object with two surfaces: one on the XY plane, that is
+    specularly reflective, and one slightly below (negative z), that is opaque.
+    The reflective surface is by default also apaque; it is a ReflectiveReceiver
+    object, so all hits can be obtained after a trace using that surface's 
+    get_all_hits() method.
+    
+    Arguments:
+    width - the extent along the x axis in the local frame.
+    height - the extent along the y axis in the local frame.
+    absorptivity - the ratio of energy incident on the reflective side that's
+        not reflected back.
+    
+    Returns:
+    front - the receiving surface
+    obj - the AssembledObject containing both surfaces
+    """
+    front = Surface(RectPlateGM(width, height), 
+        opt.ReflectiveReceiver(absorptivity))
+    back = Surface(RectPlateGM(width, height), opt.gen_reflective(1.),
+        location=r_[0., 0., -1e-10])
+    obj = AssembledObject(surfs=[front, back])
+    return front, obj
