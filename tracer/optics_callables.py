e93de97d84f335f5121f4d31905e2a7f3ed722c3
Yosef Meller <mellerf@netvision.net.il>
2009-12-31 15:11:50 +0200

    Made the reflective optics manager an object, for possible future use with multiprocessing.py

--- tracer/optics_callables.py
+++ tracer/optics_callables.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # A collection of callables and tools for creating them, that may be used for
 # the optics-callable part of a Surface object.
 
@@ -5,7 +6,7 @@ import optics
 import ray_bundle
 import numpy as N
 
-def gen_reflective(absorptivity):
+class gen_reflective(object):
     """
     Generates a function that represents the optics of an opaque, absorptive
     surface with specular reflections.
@@ -16,18 +17,20 @@ def gen_reflective(absorptivity):
     Returns:
     refractive - a function with the signature required by Surface.
     """
-    def reflective(geometry, rays, selector):
+    def __init__(self, absorptivity):
+        self._abs = absorptivity
+    
+    def __call__(self, geometry, rays, selector):
         outg = ray_bundle.RayBundle()
         outg.set_vertices(geometry.get_intersection_points_global(selector))
         outg.set_directions(optics.reflections(
             rays.get_directions()[:,selector],
             geometry.get_normals(selector)))
-        outg.set_energy(rays.get_energy()[selector]*(1 - absorptivity))
+        outg.set_energy(rays.get_energy()[selector]*(1 - self._abs))
         outg.set_parent(N.where(selector)[0]) # Each ray is reflected in order
         # Moving in the same medium, no change of ref_index
         outg.set_ref_index(rays.get_ref_index()[selector])
         return outg
-    return reflective
 
 perfect_mirror = gen_reflective(0)
 
