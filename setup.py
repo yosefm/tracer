60150367a61ee7116619210e94886e343b0c5355
Yosef Meller <mellerf@netvision.net.il>
2010-04-17 19:05:41 +0300

    Source distribution machinery

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Tracer',
+      version='0.1',
+      description='Ray-tracing library in Python, focused on solar energy research',
+      author='The Tracer developers',
+      author_email='tracer-user@lists.berlios.de',
+      url='http://tracer.berlios.de',
+      packages=['tracer', 'tracer.models'],
+      license="GPL v3.0 or later, see LICENSE file"
+     )
+
+
