e74e5bf119cdfb0950cae5f6fa1e7db85e2dde4c
Yosef Meller <mellerf@netvision.net.il>
2011-04-12 10:54:04 +0300

    New surface geometry: triangular face

---
+++ tests/test_triangular_face.py
@@ -0,0 +1,37 @@
+# Unit tests for the tracer.triangular_face module.
+
+import unittest
+import numpy as np
+
+from tracer.triangular_face import TriangularFace
+from tracer.ray_bundle import RayBundle
+from tracer.spatial_geometry import translate, rotx
+
+class TestExclusion(unittest.TestCase):
+    def setUp(self):
+        x, y = np.mgrid[-1:1:2j, -1:1:3j]
+        pos = np.vstack((x.flatten(), y.flatten(), np.ones(6)*10))
+        pos[1] -= 10.
+        direct = np.tile(np.c_[[0., 10., -10.]], (1, 6))
+        self.bund = RayBundle(pos, direct)
+    
+        verts = np.zeros((3,2))
+        verts[1] = 2.
+        verts[0] = np.r_[4., -4.]
+        
+        self.tri = TriangularFace(verts)
+    
+    def test_default_frame(self):
+        """A triangle whose frame is the global frame excludes rays correctly"""
+        prm = self.tri.find_intersections(np.eye(4), self.bund)
+        
+        np.testing.assert_array_equal(np.isfinite(prm),
+            np.r_[False, False, True, False, False, True])
+    
+    def test_transformed(self):
+        """A transformed triangle excludes rays correctly"""
+        frame = np.dot(translate(0, -1, 0.5), rotx(np.pi/4.))
+        prm = self.tri.find_intersections(frame, self.bund)
+        
+        np.testing.assert_array_equal(np.isfinite(prm),
+            np.r_[False, False, True, False, False, True])
---
+++ tracer/triangular_face.py
@@ -0,0 +1,69 @@
+# A geometry manager for a flat surface with a triangular boundary. It's not
+# much different than the GMs in the flat_surface module, but it's important
+# enough for the case of a triangulated surface, that it gets its own module.
+# 
+# References:
+# [1] http://en.wikipedia.org/wiki/Barycentric_coordinates_(mathematics)
+# [2] http://softsurfer.com/Archive/algorithm_0105/algorithm_0105.htm
+
+import numpy as np
+from .flat_surface import FiniteFlatGM
+
+class TriangularFace(FiniteFlatGM):
+    """
+    Trim the infinite flat surface with a triangle. The triangle is formed by
+    the origin, and two user-defined points on the XY plane. the plane normal
+    is selected to for a right-handed systen with the two points in the order
+    they were given.
+    """
+    
+    def __init__(self, verts):
+        """
+        Arguments:
+        verts - a 3x2 array, each column a triangle vertex, in CCW order from
+            the origin, on the XY plane. The 3rd vertex is the origin.
+        """
+        self._verts = verts
+        FiniteFlatGM.__init__(self)
+    
+    def find_intersections(self, frame, ray_bundle):
+        """
+        Register the working frame and ray bundle, calculate intersections
+        and save the parametric locations of intersection on the surface.
+        plane intersection is done in the base class, triangle trimming done
+        here.
+        
+        In this class, global coordinates of intersection points
+        are calculated and kept. _global is handled in select_rays().
+        
+        Arguments:
+        frame - the current frame, represented as a homogenous transformation
+            matrix stored in a 4x4 array.
+        ray_bundle - a RayBundle object with the incoming rays' data.
+        
+        Returns:
+        A 1D array with the parametric position of intersection along each of
+            the rays. Rays that missed the surface return +infinity.
+        """
+        ray_prms = FiniteFlatGM.find_intersections(self, frame, ray_bundle)
+        
+        # Transform the charachteristic vertices to the global systenm, then
+        # project the global intersection points to get barycentric
+        # coordinates, see [1, 2]
+        glob_verts = np.dot(frame, np.vstack(( self._verts, np.array([1,1]) )) )
+        rel_glob = glob_verts[:3].T - frame[:3,3]
+        w = self._global.T - frame[:3,3]
+        
+        uv = np.dot(self._verts[:,0], self._verts[:,1])
+        rel_dots = np.dot(w, rel_glob.T)
+        norms_sq = np.sum(self._verts**2, axis=0)
+        
+        bc = (uv*rel_dots[:,::-1] - norms_sq[::-1]*rel_dots) / \
+            (uv**2 - norms_sq[0]*norms_sq[1])
+        
+        # Use barycentric coordinates for exclusion test.
+        outside = np.any(bc < 0, axis=1) | (bc.sum(axis=1) > 1)
+        ray_prms[outside] = np.inf
+        
+        return ray_prms
+        
