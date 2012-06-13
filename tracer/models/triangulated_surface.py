f3aecc5b073fda6a3e6c8f38c0dd628944ba0b9c
Yosef Meller <mellerf@netvision.net.il>
2012-06-13 13:12:43 +0300

    Almost-stub of move_vertices() and saving of related data in __init__

--- tracer/models/triangulated_surface.py
+++ tracer/models/triangulated_surface.py
@@ -24,6 +24,9 @@ class TriangulatedSurface(AssembledObject):
             matrix of this object relative to the coordinate system of its 
             container
         """
+        self._verts = vertices
+        self._topo = faces
+        
         pos = vertices[faces[:,0]]
         edges = vertices[faces[:,1:],:] - pos[:,None,:]
         edge_norms = np.sqrt(np.sum(edges**2, axis=2))
@@ -35,6 +38,7 @@ class TriangulatedSurface(AssembledObject):
         zs = np.cross(xs, edges[:,1])
         zs /= np.sqrt((zs**2).sum(-1))[:,None]
         non_collinear = np.any(abs(zs) > 1e-6, axis=1)
+        
         xs = xs[non_collinear]
         zs = zs[non_collinear]
         ys = np.cross(zs, xs)
@@ -44,9 +48,35 @@ class TriangulatedSurface(AssembledObject):
         edges_local = np.sum(rots.transpose(0,2,1)[:,None,...]*\
             edges[:,:,None,:], axis=3)
         
-        face_list = [Surface(TriangularFace(edges_local[face_ix].T), optics,
-            location=pos[face_ix], rotation=rots[face_ix]) \
-            for face_ix in xrange(xs.shape[0])]
+        self._fl = [None]*faces.shape[0]
+        real_faces = non_degenerate
+        real_faces[~non_colinear] = False
         
+        for face_ix in np.nonzero(real_faces)[0]:
+            new_face = Surface(TriangularFace(edges_local[face_ix].T), optics,
+                location=pos[face_ix], rotation=rots[face_ix])
+            self._fl[face_ix] = new_face
+        
+        face_list = [s for s in self._fl if s is not None]
         AssembledObject.__init__(self, face_list, None, transform)
+
+    def move_vertices(self, vert_ix, new_verts):
+        """
+        Replaces some vertex positions with new positions, without altering the
+        topology. Edits the faces touching the moved vertex. If colinearity or
+        degeneracy changes, adds or removes a surface appropriately.
+        
+        Arguments:
+        vert_ix - an array of n indices of the vertices to replace, referes to
+            the vertices given to the constructor.
+        new_verts - an (n,3) array, row i is a new vertex position for the
+            vertex whose index is vert_ix i.
+        """
+        self._verts[vert_ix] = new_verts
+        
+        # Update faces:
+        update = np.nonzero(np.any(
+            np.any(faces[...,None] == vert_ix[None,None,:], axis=-1), axis=-1))[0]
+        pos = vertices[faces[update,0]]
+        edges = vertices[faces[update,1:],:] - pos[:,None,:]
 
